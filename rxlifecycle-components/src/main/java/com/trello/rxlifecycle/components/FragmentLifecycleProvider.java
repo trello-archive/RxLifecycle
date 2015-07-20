48d0d74ef608770d9750a6bb6e584268be0278d4
Dan Lew <danlew42@gmail.com>
2015-07-20 15:07:44 -0500

    Spruced up Javadoc
    Fixed some errors and added docs to methods lacking them.

--- rxlifecycle-components/src/main/java/com/trello/rxlifecycle/components/FragmentLifecycleProvider.java
+++ rxlifecycle-components/src/main/java/com/trello/rxlifecycle/components/FragmentLifecycleProvider.java
@@ -11,10 +11,28 @@ import rx.Observable;
  */
 public interface FragmentLifecycleProvider {
 
+    /**
+     * @return a sequence of {@link android.app.Fragment} lifecycle events
+     */
     Observable<FragmentEvent> lifecycle();
 
+    /**
+     * Binds a source until a specific {@link FragmentEvent} occurs.
+     * <p>
+     * Intended for use with {@link Observable#compose(Observable.Transformer)}
+     *
+     * @param event the {@link FragmentEvent} that triggers unsubscription
+     * @return a reusable {@link rx.Observable.Transformer} which unsubscribes when the event triggers.
+     */
     <T> Observable.Transformer<T, T> bindUntilEvent(FragmentEvent event);
 
+    /**
+     * Binds a source until the next reasonable {@link FragmentEvent} occurs.
+     * <p>
+     * Intended for use with {@link Observable#compose(Observable.Transformer)}
+     *
+     * @return a reusable {@link rx.Observable.Transformer} which unsubscribes at the correct time.
+     */
     <T> Observable.Transformer<T, T> bindToLifecycle();
 
 }
