c27d717c65726d27f99dd22719eb6904e800fe14
Anton Danshin <adanshin@avito.ru>
2015-09-29 15:29:46 +0300

    fixing tests...

--- rxlifecycle-components/src/test/java/com/trello/rxlifecycle/components/RxFragmentLifecycleTest.java
+++ rxlifecycle-components/src/test/java/com/trello/rxlifecycle/components/RxFragmentLifecycleTest.java
@@ -62,7 +62,7 @@ public class RxFragmentLifecycleTest {
         TestSubscriber<FragmentEvent> testSubscriber = new TestSubscriber<>();
         provider.lifecycle().skip(1).subscribe(testSubscriber);
 
-        fragment.onAttach(null);
+        fragment.onAttach((Context) null);
         fragment.onCreate(null);
         fragment.onViewCreated(null, null);
         fragment.onStart();
