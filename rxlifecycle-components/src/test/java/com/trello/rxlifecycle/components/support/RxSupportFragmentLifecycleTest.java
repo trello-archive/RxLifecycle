e09452163b137c02fa4f560ec7ed6308fff04f7a
Anton Danshin <adanshin@avito.ru>
2015-09-29 15:13:57 +0300

    - updated return generic types for Transformer to better support Kotlin type inference - changed deprecated `Fragment.onAttach(Activity)` to `Fragment.onAttach(Context)`

--- rxlifecycle-components/src/test/java/com/trello/rxlifecycle/components/support/RxSupportFragmentLifecycleTest.java
+++ rxlifecycle-components/src/test/java/com/trello/rxlifecycle/components/support/RxSupportFragmentLifecycleTest.java
@@ -14,6 +14,7 @@
 
 package com.trello.rxlifecycle.components.support;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import com.trello.rxlifecycle.FragmentEvent;
@@ -62,7 +63,7 @@ public class RxSupportFragmentLifecycleTest {
         TestSubscriber<FragmentEvent> testSubscriber = new TestSubscriber<>();
         provider.lifecycle().skip(1).subscribe(testSubscriber);
 
-        fragment.onAttach(null);
+        fragment.onAttach((Context) null);
         fragment.onCreate(null);
         fragment.onViewCreated(null, null);
         fragment.onStart();
@@ -117,7 +118,7 @@ public class RxSupportFragmentLifecycleTest {
         Fragment fragment = (Fragment) provider;
         startFragment(fragment);
 
-        fragment.onAttach(null);
+        fragment.onAttach((Context) null);
         TestSubscriber<Object> attachTestSub = new TestSubscriber<>();
         observable.compose(provider.bindToLifecycle()).subscribe(attachTestSub);
 
