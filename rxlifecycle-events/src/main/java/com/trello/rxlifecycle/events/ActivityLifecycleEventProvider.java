58001cf6596d09d55d0e12419321525cac79645a
Steffan Davies <steff@steffandroid.co.uk>
2015-10-07 22:51:55 +0100

    Add Javadoc

--- rxlifecycle-events/src/main/java/com/trello/rxlifecycle/events/ActivityLifecycleEventProvider.java
+++ rxlifecycle-events/src/main/java/com/trello/rxlifecycle/events/ActivityLifecycleEventProvider.java
@@ -4,6 +4,9 @@ import rx.Observable;
 
 public interface ActivityLifecycleEventProvider {
 
+    /**
+     * @return a sequence of {@link com.trello.rxlifecycle.events.ActivityResultEvent} events
+     */
     Observable<ActivityResultEvent> activityResult();
 
 }
--- rxlifecycle-events/src/main/java/com/trello/rxlifecycle/events/FragmentLifecycleEventProvider.java
+++ rxlifecycle-events/src/main/java/com/trello/rxlifecycle/events/FragmentLifecycleEventProvider.java
@@ -4,6 +4,9 @@ import rx.Observable;
 
 public interface FragmentLifecycleEventProvider {
 
+    /**
+     * @return a sequence of {@link com.trello.rxlifecycle.events.ActivityResultEvent} events
+     */
     Observable<ActivityResultEvent> activityResult();
 
 }
