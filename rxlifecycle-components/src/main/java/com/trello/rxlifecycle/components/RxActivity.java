b0b68a78110e8b4fb2a2c6e8a2297b9b8aabf7ca
Niklas Baudy <niklas.baudy@vanniktech.de>
2015-11-05 14:29:18 +0100

    Add CallSuper annotation to provided Activity, Context and Dialog components

--- rxlifecycle-components/src/main/java/com/trello/rxlifecycle/components/RxActivity.java
+++ rxlifecycle-components/src/main/java/com/trello/rxlifecycle/components/RxActivity.java
@@ -2,8 +2,11 @@ package com.trello.rxlifecycle.components;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.CallSuper;
+
 import com.trello.rxlifecycle.ActivityEvent;
 import com.trello.rxlifecycle.RxLifecycle;
+
 import rx.Observable;
 import rx.subjects.BehaviorSubject;
 
@@ -27,36 +30,42 @@ public class RxActivity extends Activity implements ActivityLifecycleProvider {
     }
 
     @Override
+    @CallSuper
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         lifecycleSubject.onNext(ActivityEvent.CREATE);
     }
 
     @Override
+    @CallSuper
     protected void onStart() {
         super.onStart();
         lifecycleSubject.onNext(ActivityEvent.START);
     }
 
     @Override
+    @CallSuper
     protected void onResume() {
         super.onResume();
         lifecycleSubject.onNext(ActivityEvent.RESUME);
     }
 
     @Override
+    @CallSuper
     protected void onPause() {
         lifecycleSubject.onNext(ActivityEvent.PAUSE);
         super.onPause();
     }
 
     @Override
+    @CallSuper
     protected void onStop() {
         lifecycleSubject.onNext(ActivityEvent.STOP);
         super.onStop();
     }
 
     @Override
+    @CallSuper
     protected void onDestroy() {
         lifecycleSubject.onNext(ActivityEvent.DESTROY);
         super.onDestroy();
--- rxlifecycle-components/src/main/java/com/trello/rxlifecycle/components/RxFragment.java
+++ rxlifecycle-components/src/main/java/com/trello/rxlifecycle/components/RxFragment.java
@@ -2,9 +2,12 @@ package com.trello.rxlifecycle.components;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.CallSuper;
 import android.view.View;
+
 import com.trello.rxlifecycle.FragmentEvent;
 import com.trello.rxlifecycle.RxLifecycle;
+
 import rx.Observable;
 import rx.subjects.BehaviorSubject;
 
@@ -28,60 +31,70 @@ public class RxFragment extends Fragment implements FragmentLifecycleProvider {
     }
 
     @Override
+    @CallSuper
     public void onAttach(android.app.Activity activity) {
         super.onAttach(activity);
         lifecycleSubject.onNext(FragmentEvent.ATTACH);
     }
 
     @Override
+    @CallSuper
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         lifecycleSubject.onNext(FragmentEvent.CREATE);
     }
 
     @Override
+    @CallSuper
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         lifecycleSubject.onNext(FragmentEvent.CREATE_VIEW);
     }
 
     @Override
+    @CallSuper
     public void onStart() {
         super.onStart();
         lifecycleSubject.onNext(FragmentEvent.START);
     }
 
     @Override
+    @CallSuper
     public void onResume() {
         super.onResume();
         lifecycleSubject.onNext(FragmentEvent.RESUME);
     }
 
     @Override
+    @CallSuper
     public void onPause() {
         lifecycleSubject.onNext(FragmentEvent.PAUSE);
         super.onPause();
     }
 
     @Override
+    @CallSuper
     public void onStop() {
         lifecycleSubject.onNext(FragmentEvent.STOP);
         super.onStop();
     }
 
     @Override
+    @CallSuper
     public void onDestroyView() {
         lifecycleSubject.onNext(FragmentEvent.DESTROY_VIEW);
         super.onDestroyView();
     }
 
     @Override
+    @CallSuper
     public void onDestroy() {
         lifecycleSubject.onNext(FragmentEvent.DESTROY);
         super.onDestroy();
     }
 
     @Override
+    @CallSuper
     public void onDetach() {
         lifecycleSubject.onNext(FragmentEvent.DETACH);
         super.onDetach();
