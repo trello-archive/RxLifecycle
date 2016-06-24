29cd35a3f669e40c743ae661fabea1bc8b62c83e
Mike Worth <mike@slack-corp.com>
2016-06-24 11:59:15 -0700

    Subscriptions using bindToLifecycle during onCreateView should unsubscribe in onDestroyView and not in onDestroy

--- rxlifecycle-components/src/main/java/com/trello/rxlifecycle/components/RxDialogFragment.java
+++ rxlifecycle-components/src/main/java/com/trello/rxlifecycle/components/RxDialogFragment.java
@@ -6,7 +6,10 @@ import android.support.annotation.CallSuper;
 import android.support.annotation.CheckResult;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+
 import com.trello.rxlifecycle.FragmentEvent;
 import com.trello.rxlifecycle.FragmentLifecycleProvider;
 import com.trello.rxlifecycle.LifecycleTransformer;
@@ -53,11 +56,13 @@ public class RxDialogFragment extends DialogFragment implements FragmentLifecycl
         lifecycleSubject.onNext(FragmentEvent.CREATE);
     }
 
+    @Nullable
     @Override
     @CallSuper
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
         lifecycleSubject.onNext(FragmentEvent.CREATE_VIEW);
+        return view;
     }
 
     @Override
